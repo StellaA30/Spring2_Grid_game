1d03ebf46e82fd97373ed8eb8a23f777ef6c0633
StellaA30 <56633439+StellaA30@users.noreply.github.com>
2023-10-27 16:12:30 +0100

    adds GridTest

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -1,2 +1,34 @@
+import org.example.Grid;
+import org.example.Monster;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
 public class GridTest {
+
+    private Grid grid;
+
+    @Mock
+    private Monster monster1;
+
+    @Mock
+    private Monster monster2;
+
+    @Mock
+    private Monster monster3;
+
+    @BeforeEach
+    public void setUp(){
+        grid = new Grid(6, 6);
+
+    }
+
+
+
+
 }
+
+
